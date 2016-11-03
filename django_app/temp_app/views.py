ea5c9dfe199c3a10ad18b9f7fc06db2115c599b4
BethMwangi <wanjikumwangib@gmail.com>
2016-11-03 20:52:10 +0300

    line chart works and can get data from db

--- django_app/temp_app/views.py
+++ django_app/temp_app/views.py
@@ -6,6 +6,12 @@ from django.views.decorators.csrf import csrf_protect
 import datetime
 from django import forms
 from django.utils import timezone
+from random import randint
+from django.views.generic import TemplateView
+from chartjs.views.lines import BaseLineChartView
+from chartit import DataPool, Chart
+
+
 
 def main_page(request):
 
@@ -25,21 +31,47 @@ def main_page(request):
 			})
 
 
+# def list_temp(request):
+# 	temperatures = TempChart.objects.all()
+# 	return render_to_response('list_temp.html', {'temperatures': temperatures})
+# 	# return HttpResponse(json.dumps(data))
+
+
 def list_temp(request):
-	temperatures = TempChart.objects.all()
-	return render_to_response('list_temp.html', {'temperatures': temperatures})
+	# crete a datapool with the data we want to retrieve
+	tempdata = DataPool(
+		series= [{'options': {
+		'source':TempChart.objects.all()},
+		'terms': [
+		'city_name',
+		'temperature',
+		'created_date']}
+		])
+	
+	# create chart object
+
+	cht = Chart(
+		datasource = tempdata,
+		series_options = 
+		[{'options':{
+		'type': 'line',
+		'stacking': False},
+		'terms':{
+		'city_name': [
+		'temperature',
+		'created_date']
+		}}],
+		chart_options = 
+		{'title': {
+		'text': 'Temp Data by City_name'},
+		'xAxis': {
+		'title' : {
+		'text': 'City_name'}}})
 
 
-
-
-
-
-
-
-
-
-
-
+	# Send the chart object to the template.
+	# return render_to_response({'list_temp': cht})
+	return render_to_response('list_temp.html', {'tempchart':cht})
 
 
 
